57b9b5ecd8ab3a80f0a88f083c5dde43bba33000
Qian Yilue <fengzhaqiQYQC@outlook.com>
2022-10-11 00:44:20 +0800

    update assignment 1 solution

--- Assignment_1/solutions/task0/task0.1.py
+++ Assignment_1/solutions/task0/task0.1.py
@@ -21,15 +21,18 @@ def sol4(list_of_lists):
     return flattened_list
 
 if __name__ == "__main__":
-    list_of_lists = [list(range(1000))] * 1000
+    list_of_lists = [list(range(1000)) for _ in range(1000)]
     for i in range(1, 5):
-        start_time = time.perf_counter()
-        locals()['sol' + str(i)](list_of_lists)
-        end_time = time.perf_counter()
-        print('Solution ' + str(i) + ' took ' + str(end_time - start_time) + ' seconds')
-
-
-# Solution 1 took 0.02140442800009623 seconds
-# Solution 2 took 0.8658737830000973 seconds
-# Solution 3 took 0.01031219600008626 seconds
-# Solution 4 took 0.00475121300041792 seconds
+        tot_time = 0
+        for _ in range(10):
+            start = time.time()
+            eval(f"sol{i}(list_of_lists)")
+            end = time.time()
+            tot_time += end - start
+        print(f"Solution {i} took {tot_time / 10.} seconds")
+       
+       
+# Solution 1 took 0.0214430570602417 seconds
+# Solution 2 took 1.9747170209884644 seconds
+# Solution 3 took 0.010944032669067382 seconds
+# Solution 4 took 0.006340336799621582 seconds
